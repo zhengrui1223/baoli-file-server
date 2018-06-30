849519154e804f6719c88572b713c7181568ebbb
zhengrui <zhengrui@jiangduoduo.com>
2018-07-01 01:16:48 +0800

    redis

--- src/test/java/service/RedisTest.java
+++ src/test/java/service/RedisTest.java
@@ -14,24 +14,29 @@ import java.util.*;
 public class RedisTest {
 
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        Jedis jedis = new Jedis("47.98.147.110", 6379);
         //权限认证
         jedis.auth("Jiao1223Yu!");
 
+        generatorSortedSet(jedis);
 
-        Map<String, Double> map = new HashMap<>();
-        map.put("a", 1d);
-        map.put("a1", 1d);
-        map.put("a2", 1d);
-        map.put("b", 2d);
-        map.put("c", 3d);
-        map.put("d", 4d);
-
-        jedis.zadd("testLimit", map);
-
-        Set<String> testLimit = jedis.zrevrangeByScore("testLimit", 4d, 1d, 0, 4);
+        Long count = jedis.zcount("sortedSet", 93d, 100d);
+        System.out.println("count: " + count);
+        int start = (count.intValue() / 10 - 1) * 10 + count.intValue() % 10;
+        System.out.println("start: " + start);
+        Set<String> testLimit = jedis.zrevrangeByScore("sortedSet", 100d, 93d, start < 0 ? 0 : start, 10);
         System.out.println(testLimit);
+    }
 
+    private static void generatorSortedSet(Jedis jedis) {
+        Date now = new Date();
+        Map<String, Double> map = new HashMap<>();
+        for (int i=0; i< 100; i++) {
+            //map.put("test" + i, now.getTime() / 1000 + i + 0d);
+            map.put("test" + i, i + 0d);
+        }
+
+        jedis.zadd("sortedSet", map);
 
     }
 
